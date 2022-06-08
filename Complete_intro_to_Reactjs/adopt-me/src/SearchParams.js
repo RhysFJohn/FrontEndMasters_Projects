9ebda6f7d9e075fe1eb5c9361177323734aa71e7
RhysFJohn <r.freeman-john@outlook.com>
2022-06-08 12:39:45 +0100

    added a SearchParams.js file

---
+++ Complete_intro_to_Reactjs/adopt-me/src/SearchParams.js
@@ -0,0 +1,17 @@
+
+
+const SearchParams = () => {
+  const location = "London, GB";
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          <input id="location" value={location} placeholder="location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default SearchParams
