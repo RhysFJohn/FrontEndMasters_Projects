f8fe9492d06dcc1c25f0ef80d0049b870338c9c6
RhysFJohn <r.freeman-john@outlook.com>
2022-06-09 16:13:38 +0100

    Fetching Data in componentDidMount

--- Complete_intro_to_Reactjs/adopt-me/src/Details.js
+++ Complete_intro_to_Reactjs/adopt-me/src/Details.js
@@ -8,6 +8,16 @@ class Details extends Component {
     this.start = { loading: true };
   }
 
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
   render() {
     if (this.start.loading) {
       return <h2>Loading...</h2>;
@@ -30,4 +40,9 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
